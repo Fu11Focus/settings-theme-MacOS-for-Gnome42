d0941a7dda7839931342b256e9c63fd6ad31ccba
Fu11Focus <evgen_k9@ukr.net>
2022-08-15 20:36:47 +0300

    add fonts

--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@
 
 mkdir ~/.themes
 mkdir ~/.icons
+mkdir ~/.local/share/fonts/
 
 cp -r cursor-MacOS ~/.icons/
 cp -r icons-Cupertino-Ventura ~/.icons/
 
 cp -r Mojave-Light ~/.themes/
 cp -r Mojave-Dark ~/.themes/
+
+cp FontsFree-Net-SFProDisplay-Regular.ttf ~/.local/share/fonts/
 
 #install and settings Fildem Global Menu
 sudo apt install plank gnome-shell-extensions gnome-tweaks bamfdaemon gir1.2-bamf-3 libbamf3-2 libkeybinder-3.0-0 gir1.2-keybinder-3.0 appmenu-gtk2-module appmenu-gtk3-module unity-gtk-module-common python3-pip git
@@ -17,6 +20,7 @@ python3 -m pip install --user fuzzysearch
 cd Fildem-0.6.7
 sudo python3 setup.py install --optimize=1
 cp -r fildemGMenu@gonza.com ~/.local/share/gnome-shell/extensions/
+cd ..
 cp metadata.json ~/.local/share/gnome-shell/extensions/fildemGMenu@gonza.com/metadata.json
 cp .gtkrc-2.0 ~/.gtkrc-2.0
 cp settings.ini ~/.config/gtk-3.0/settings.ini
@@ -24,4 +28,5 @@ cp fildem.desktop ~/.config/autostart/fildem.desktop
 
 cp -r plank-MacOS-theme ~/.local/share/plank/themes/
 
+cp big-sur-wallpaper.jpg ~/Pictures/
 
